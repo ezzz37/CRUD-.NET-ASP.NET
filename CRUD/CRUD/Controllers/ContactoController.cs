7d9afc1c151eaf06d2c2b1bacd858cceaae08ba7
ezzz3 <ezequielpretti@hotmail.com>
2025-05-13 14:09:13 -0300

    declare vrs de conexion a la db se cambiaron los model, controller y vistas

---
+++ CRUD/CRUD/Controllers/ContactoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private static string conexion = ConfigurationManager.ConnectionStrings["cadena"].ToString();
+        private static List<Contacto> contactos = new List<Contacto>();
+        // GET: Contacto
+        public ActionResult Inicio()
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Contacto", oconexion);
+                cmd.CommandType = CommandType.Text;
+                oconexion.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+
+                }
+            }
+                return View();
+        }
+    }
+}
